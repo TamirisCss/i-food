7234eec304952213401df3601a11bb05f7a4d918
Tamiris Cristine <tamiris.css@gmail.com>
2024-06-07 10:00:41 +0200

    feat: add restaurant list component

---
+++ app/_components/restaurant-item.tsx
@@ -0,0 +1,59 @@
+import { Restaurant } from "@prisma/client";
+import { Bike, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
+import Image from "next/image";
+import { formatCurrency } from "../_helpers/price";
+import { Button } from "./ui/button";
+
+interface RestaurantItemProps {
+  restaurant: Restaurant;
+}
+
+const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
+  return (
+    <div className="min-w-[266px] max-w-[266px] space-y-3">
+      <div className="w--full relative h-[136px]">
+        <Image
+          src={restaurant.imageUrl}
+          fill
+          className="rounded-lg object-cover"
+          alt={restaurant.name}
+        />
+
+        {/* TODO: REVIEW LOGIC */}
+        <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-white px-2 py-[2px]">
+          <StarIcon size={12} className="fill-yllow-400 text-yellow-400" />
+          <span>5.0</span>
+        </div>
+
+        {/* TODO: FAVORITE LOGIC */}
+        <Button
+          size="icon"
+          className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
+        >
+          <HeartIcon size={16} className="fill-white" />
+        </Button>
+      </div>
+      <div>
+        <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+        <div className="flex gap-3">
+          <div className="flex items-center gap-1">
+            <Bike size={14} className="text-primary" />
+            <span className="text-xs text-muted-foreground">
+              {Number(restaurant.deliveryFee) === 0
+                ? "Entrega grátis"
+                : formatCurrency(Number(restaurant.deliveryFee))}
+            </span>
+          </div>
+          <div className="flex items-center gap-1">
+            <TimerIcon size={14} className="text-primary" />
+            <span className="text-xs text-muted-foreground">
+              {restaurant.deliveryTimeMinutes} min
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantItem;
