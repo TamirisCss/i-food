b16715325794cc25f9c07ecc3738f35c1f346f88
Tamiris Cristine <tamiris.css@gmail.com>
2024-06-11 19:36:32 +0200

    feat: add category product page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,14 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="FSW-food-logo" height={30} width={100} />
+      <div className="relative h-[30px] w-[100px]">
+        <Image
+          src="/logo.png"
+          alt="FSW-food-logo"
+          fill
+          className="object-contain"
+        />
+      </div>
       <Button
         size="icon"
         variant="outline"
